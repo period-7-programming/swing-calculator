bf48eccc29b31b2ae426a9eaf431da674396a105
OliverDalmi <oliverdalmi@hotmail.com>
2017-02-09 14:10:50 -0600

    Current Version
    Everything works, need to fix something for negative numbers, nested
    parenthesis should work but do not.

---
+++ Oliver/main.java
@@ -0,0 +1,341 @@
+package calculator;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class main extends JFrame {
+	int[] buttonX = { 7, 77, 147, 217, 7, 77, 147, 217, 7, 77, 147, 217, 7, 77, 147, 217, 7, 77, 147 }; // coordinates
+	int[] buttonY = { 75, 75, 75, 75, 135, 135, 135, 135, 195, 195, 195, 195, 255, 255, 255, 255, 315, 315, 315 };
+	int[] buttonWidth = { 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 60, 130 };
+	String[] buttonText = { "1", "2", "3", "*", "4", "5", "6", "/", "7", "8", "9", "+", ".", "0", "\u232b", "-", "(",
+			")", "=", }; // defines button text
+	int i;
+	JButton buttons[] = new JButton[19];
+
+	public static void main(String[] args) {
+		main w = new main();
+		w.setVisible(true);
+	}
+
+	public main() {
+		super();
+		this.setSize(300, 400);
+		this.getContentPane().setLayout(null);
+		JTextField equationDisplay = new JTextField();
+		;
+		equationDisplay.setBounds(7, 7, 270, 61);
+		this.add(equationDisplay);
+		for (i = 0; i < buttonX.length; i++) {
+			JButton jButton = new javax.swing.JButton();
+			jButton.setBounds(buttonX[i], buttonY[i], buttonWidth[i], 40);
+			jButton.setText(buttonText[i]);
+			buttons[i] = jButton;
+			this.add(buttons[i]);
+			ActionListener listen;
+			if (i < 18 && i != 14) {
+				listen = new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						equationDisplay.setText(equationDisplay.getText() + ((JButton) arg0.getSource()).getText());
+					}
+				};
+			} else if (i == 14) {
+				listen = new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						equationDisplay.setText(
+								equationDisplay.getText().substring(0, equationDisplay.getText().length() - 1));
+					}
+				};
+			} else {
+				listen = new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						equationDisplay.setText(calculate(equationDisplay.getText()));
+					}
+				};
+			}
+			buttons[i].addActionListener(listen);
+		}
+		this.setTitle("Calculator");
+	}
+
+	String calculate(String equation) {
+		if (equation.contains("(")) {
+			String result = calculate(equation.substring(equation.indexOf("(") + 1 , equation.indexOf(")")));
+			equation = equation.substring(0, equation.indexOf("("))
+					+ result + equation.substring(equation.indexOf(")") + 1);
+		}
+
+		if (equation.contains("*") || equation.contains("/")) {
+			if (equation.contains("*") && equation.contains("/")) {
+				if (equation.indexOf("*") < equation.indexOf("/")) {
+					String firstHalf = equation.substring(0, equation.indexOf("*"));
+					int startNumOne = 0;
+					if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+						startNumOne = firstHalf.indexOf("+");
+					}
+					if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+						startNumOne = firstHalf.indexOf("-");
+					}
+					if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+						startNumOne = firstHalf.indexOf("*");
+					}
+					if (firstHalf.contains("/") && firstHalf.indexOf("/") > startNumOne) {
+						startNumOne = firstHalf.indexOf("/");
+					}
+					String secondHalf = equation.substring(equation.indexOf("*") + 1);
+					int endNumTwo = secondHalf.length();
+					if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("+");
+					}
+					if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("-");
+					}
+					if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("*");
+					}
+					if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("/");
+					}
+					float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("*")))
+							* Float.parseFloat(equation.substring(equation.indexOf("*") + 1, endNumTwo + 1 + equation.indexOf("*")));
+					equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+							+ equation.substring(endNumTwo + 1 + equation.indexOf("*"));
+				} else {
+					String firstHalf = equation.substring(0, equation.indexOf("/"));
+					int startNumOne = 0;
+					if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+						startNumOne = firstHalf.indexOf("+");
+					}
+					if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+						startNumOne = firstHalf.indexOf("-");
+					}
+					if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+						startNumOne = firstHalf.indexOf("*");
+					}
+					if (firstHalf.contains("/") && firstHalf.indexOf("/") > startNumOne) {
+						startNumOne = firstHalf.indexOf("/");
+					}
+					String secondHalf = equation.substring(equation.indexOf("/") + 1);
+					int endNumTwo = secondHalf.length();
+					if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("+");
+					}
+					if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("-");
+					}
+					if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("*");
+					}
+					if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("/");
+					}
+					float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("/")))
+							/ Float.parseFloat(equation.substring(equation.indexOf("/") + 1, endNumTwo + 1 + equation.indexOf("/")));
+					equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+							+ equation.substring(endNumTwo + 1 + equation.indexOf("/"));
+				}
+			} else if (equation.contains("*")) {
+				String firstHalf = equation.substring(0, equation.indexOf("*"));
+				int startNumOne = 0;
+				if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+					startNumOne = firstHalf.indexOf("+");
+				}
+				if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+					startNumOne = firstHalf.indexOf("-");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("*");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("/");
+				}
+				String secondHalf = equation.substring(equation.indexOf("*") + 1);
+				int endNumTwo = secondHalf.length();
+				if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("+");
+				}
+				if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("-");
+				}
+				if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("*");
+				}
+				if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("/");
+				}
+				float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("*")))
+						* Float.parseFloat(equation.substring(equation.indexOf("*") + 1, endNumTwo + 1 + equation.indexOf("*")));
+				equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+						+ equation.substring(endNumTwo + 1 + equation.indexOf("*"));
+			} else {
+				String firstHalf = equation.substring(0, equation.indexOf("/"));
+				int startNumOne = 0;
+				if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+					startNumOne = firstHalf.indexOf("+");
+				}
+				if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+					startNumOne = firstHalf.indexOf("-");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("*");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("/");
+				}
+				String secondHalf = equation.substring(equation.indexOf("/") + 1);
+				int endNumTwo = secondHalf.length();
+				if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("+");
+				}
+				if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("-");
+				}
+				if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("*");
+				}
+				if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("/");
+				}
+				float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("/")))
+						/ Float.parseFloat(equation.substring(equation.indexOf("/") + 1, endNumTwo + 1 + equation.indexOf("/")));
+				equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+						+ equation.substring(endNumTwo + 1 + equation.indexOf("/"));
+			}
+			equation = calculate(equation);
+		} else if (equation.contains("+") || equation.contains("-")) {
+			if (equation.contains("+") && equation.contains("-")) {
+				if (equation.indexOf("+") < equation.indexOf("-")) {
+					String firstHalf = equation.substring(0, equation.indexOf("+"));
+					int startNumOne = 0;
+					if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+						startNumOne = firstHalf.indexOf("+");
+					}
+					if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+						startNumOne = firstHalf.indexOf("-");
+					}
+					String secondHalf = equation.substring(equation.indexOf("+") + 1);
+					int endNumTwo = secondHalf.length();
+					if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("+");
+					}
+					if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("-");
+					}
+					if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("*");
+					}
+					if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("/");
+					}
+					float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("+")))
+							+ Float.parseFloat(equation.substring(equation.indexOf("+") + 1, endNumTwo + 1 + equation.indexOf("+")));
+					equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+							+ equation.substring(endNumTwo + 1 + equation.indexOf("+"));
+				} else {
+					String firstHalf = equation.substring(0, equation.indexOf("-"));
+					int startNumOne = 0;
+					if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+						startNumOne = firstHalf.indexOf("+");
+					}
+					if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+						startNumOne = firstHalf.indexOf("-");
+					}
+					if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+						startNumOne = firstHalf.indexOf("*");
+					}
+					if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+						startNumOne = firstHalf.indexOf("/");
+					}
+					String secondHalf = equation.substring(equation.indexOf("-") + 1);
+					int endNumTwo = secondHalf.length();
+					if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("+");
+					}
+					if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("-");
+					}
+					if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("*");
+					}
+					if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+						endNumTwo = secondHalf.indexOf("/");
+					}
+					float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("-")))
+							- Float.parseFloat(equation.substring(equation.indexOf("-") + 1, endNumTwo + 1 + equation.indexOf("-")));
+					equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+							+ equation.substring(endNumTwo + 1 + equation.indexOf("-"));
+				}
+			} else if (equation.contains("+")) {
+				String firstHalf = equation.substring(0, equation.indexOf("+"));
+				int startNumOne = 0;
+				if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+					startNumOne = firstHalf.indexOf("+");
+				}
+				if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+					startNumOne = firstHalf.indexOf("-");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("*");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("/");
+				}
+				String secondHalf = equation.substring(equation.indexOf("+") + 1);
+				int endNumTwo = secondHalf.length();
+				if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("+");
+				}
+				if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("-");
+				}
+				if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("*");
+				}
+				if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("/");
+				}
+				float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("+")))
+						+ Float.parseFloat(equation.substring(equation.indexOf("+") + 1, endNumTwo + 1 + equation.indexOf("+")));
+				equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+						+ equation.substring(endNumTwo + 1 + equation.indexOf("+"));
+			} else {
+				String firstHalf = equation.substring(0, equation.indexOf("-"));
+				int startNumOne = 0;
+				if (firstHalf.contains("+") && firstHalf.indexOf("+") > startNumOne) {
+					startNumOne = firstHalf.indexOf("+");
+				}
+				if (firstHalf.contains("-") && firstHalf.indexOf("-") > startNumOne) {
+					startNumOne = firstHalf.indexOf("-");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("*");
+				}
+				if (firstHalf.contains("*") && firstHalf.indexOf("*") > startNumOne) {
+					startNumOne = firstHalf.indexOf("/");
+				}
+				String secondHalf = equation.substring(equation.indexOf("-") + 1);
+				int endNumTwo = secondHalf.length();
+				if (secondHalf.contains("+") && secondHalf.indexOf("+") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("+");
+				}
+				if (secondHalf.contains("-") && secondHalf.indexOf("-") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("-");
+				}
+				if (secondHalf.contains("*") && secondHalf.indexOf("*") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("*");
+				}
+				if (secondHalf.contains("/") && secondHalf.indexOf("/") < endNumTwo) {
+					endNumTwo = secondHalf.indexOf("/");
+				}
+				float calculation = Float.parseFloat(equation.substring(startNumOne, equation.indexOf("-")))
+						- Float.parseFloat(equation.substring(equation.indexOf("-") + 1, endNumTwo + 1 + equation.indexOf("-")));
+				equation = equation.substring(0, startNumOne) + Float.toString(calculation)
+						+ equation.substring(endNumTwo + 1 + equation.indexOf("-"));
+			}
+			equation = calculate(equation);
+		}
+		return equation;
+	}
+}
